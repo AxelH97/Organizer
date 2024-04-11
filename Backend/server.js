95deef10f86a885ab6a41159c9d43418e51c3074
AxelH97 <hilde.axel@web.de>
2024-04-11 13:38:34 +0200

    add post for register in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,17 @@ connectMongoDB();
 
 let tasks = [];
 
+app.post("/api/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json({ message: "Registrierung erfolgreich" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 app.get("/api/tasks", (req, res) => {
   res.json(tasks);
 });
